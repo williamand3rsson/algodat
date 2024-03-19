9aa29979c81bc9ee5675ff13e12a9baf6344d4d2
williamand3rsson <william02andersson@gmail.com>
2024-03-19 15:47:06 +0100

    sorted companyPref

--- 1stablematching/src.py
+++ 1stablematching/src.py
@@ -1,15 +1,29 @@
-with open('1stablematching/data/sample/1.in', 'r') as file:
+with open('1stablematching/data/secret/2testmid.in', 'r') as file:
     # Read the file line by line
     students = []
     studentPref = []
     companies = []
     companyPref = []
     counter = 0
+    def sortPref(prefList):
+        tempList = [0] * (len(prefList))
+        for i in range(len(prefList)):
+            tempList[prefList[i] - 1] = i + 1
+        return tempList
+    def strtoint(strList):
+        intNbrs = list(map(int, strList))
+        return intNbrs
+        
+
+
+
+
     for line in file:
         if counter == 0:
             counter += 1
         else :
             nbrs = line.split()
+            nbrs = strtoint(nbrs)
             if nbrs[0] in companies:
                 students.append(nbrs[0])
                 nbrs.pop(0)
@@ -17,4 +31,12 @@ with open('1stablematching/data/sample/1.in', 'r') as file:
             else:
                 companies.append(nbrs[0])
                 nbrs.pop(0)
+                nbrs = sortPref(nbrs)
                 companyPref.append(nbrs)
+print(companyPref)
+print(companies)
+print(studentPref)
+print(students)
+
+
+             
