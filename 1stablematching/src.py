b8d5047caa82849682a3870975955515ac883598
williamand3rsson <william02andersson@gmail.com>
2024-03-19 15:09:34 +0100

    parser done

--- 1stablematching/src.py
+++ 1stablematching/src.py
@@ -1,1 +1,20 @@
-print("Hello World")
+with open('1stablematching/data/sample/1.in', 'r') as file:
+    # Read the file line by line
+    students = []
+    studentPref = []
+    companies = []
+    companyPref = []
+    counter = 0
+    for line in file:
+        if counter == 0:
+            counter += 1
+        else :
+            nbrs = line.split()
+            if nbrs[0] in companies:
+                students.append(nbrs[0])
+                nbrs.pop(0)
+                studentPref.append(nbrs)
+            else:
+                companies.append(nbrs[0])
+                nbrs.pop(0)
+                companyPref.append(nbrs)
