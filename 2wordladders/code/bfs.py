d32b28c1e8d2cae4b49088e949d24de24bcc915a
williamand3rsson <william02andersson@gmail.com>
2024-03-27 12:34:21 +0100

    lite ändring

--- 2wordladders/code/bfs.py
+++ 2wordladders/code/bfs.py
@@ -6,15 +6,20 @@ class BFS:
         currentCount = 1
         allNeighbour = [start]
         currentNode = Node(start, wordLib)
-        while counter <= 5000:
+        notDone = [start]
+        oldNode = currentNode
+        while len(notDone) != 0:
             currentNode.visited = 1
-            counter += 1
             for neighbour in currentNode.neighbourList:
-                allNeighbour.append(neighbour)
-            currentNode = Node(allNeighbour[currentCount], wordLib)
-            if end in allNeighbour:
-                print(counter)
-                return
+                if neighbour.visited == 0:
+                    neighbour.visited = 1
+                    allNeighbour.append(neighbour)
+                oldNode = currentNode
+                currentNode = Node(allNeighbour[currentCount], wordLib)
+                if oldNode == end:
+                    print(counter)
+                    return
+                counter += 1
         else:
             print("Impossible")
 
