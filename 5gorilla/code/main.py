e6e53674d5ec75a369749ce3e32fe40dba2a6c9b
williamand3rsson <william02andersson@gmail.com>
2024-05-04 23:25:16 +0200

    main påbörjad

--- 5gorilla/code/main.py
+++ 5gorilla/code/main.py
@@ -1,6 +1,32 @@
 import sys
+from dp import Dp
 
 counter = 0
-
+letters = []
+matrix = []
+dic = {}
 for line in sys.stdin:
-    print(line)
+    lines = line.split()
+    if counter == 0:
+        for linan in lines:
+            letters.append(linan)
+        counter += 1
+    elif counter <= len(letters):
+        matrix.append(lines)
+        counter += 1
+    elif counter == len(letters):
+        for i in range(len(letters)):
+            for j in range(len(letters)):
+                dic[(letters[i], letters[j])] = matrix[i][j]
+        counter += 1
+    elif counter == len(letters) + 1:
+        counter += 1
+    else:
+        Dp.dp(line[0], line[1])
+        
+
+
+        
+
+        
+
