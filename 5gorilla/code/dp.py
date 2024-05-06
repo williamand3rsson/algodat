cd0080d143936701d1d0c202e757843e489c5b77
Totti Åkerström <to4370ak-s@student.lu.se>
2024-05-06 11:19:57 +0200

    v

--- 5gorilla/code/dp.py
+++ 5gorilla/code/dp.py
@@ -7,20 +7,19 @@ class Dp:
     #         dprec(str1, str2, letters, matrix)
     @staticmethod
     def value(i,j, matrix, letters):
-        print(matrix[letters.index(i)][letters.index(j)])
-        return matrix[letters.index(i)][letters.index(j)]
+        return int(matrix[letters.index(i)][letters.index(j)])
     
     @staticmethod
     def dprec(str1, str2, letters, matrix):
-        if str1 == 0 and str2 == 0:
+        if len(str1) == 0 and len(str2) == 0:
             return 0
-        elif str1 == 0:
+        elif len(str1) == 0:
             return -4 * len(str2)
-        elif str2 == 0:
+        elif len(str2) == 0:
             return -4 * len(str1)
         else:
-            return max(Dp.value(str1[0], str2[0], matrix, letters) + dprec(str1[1:], str2[1:], letters, matrix), 
-                -4 + Dp.dprec(str1, str2[1:], letters, matrix)
+            return max(Dp.value(str1[0], str2[0], matrix, letters) + Dp.dprec(str1[1:], str2[1:], letters, matrix), 
+                -4 + Dp.dprec(str1, str2[1:], letters, matrix),
                 -4 + Dp.dprec(str1[1:], str2, letters, matrix))
 
 
